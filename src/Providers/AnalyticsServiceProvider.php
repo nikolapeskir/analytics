28f0f277830f654dbdf3699635618d4aa12aa572
Nikola Peskir <nikola.peskir.bgd@gmail.com>
2019-11-08 23:28:38 +0100

    Initial Commit

---
+++ src/Providers/AnalyticsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Leanmachine\Analytics\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AnalyticsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+
+    }
+}
