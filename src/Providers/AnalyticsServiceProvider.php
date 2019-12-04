135e4d03b46897dc121f623bf6a9fb28d9b1779f
Nikola Peskir <nikola.peskir.bgd@gmail.com>
2019-12-04 12:52:03 +0100

    Include Migrations in Provider

--- src/Providers/AnalyticsServiceProvider.php
+++ src/Providers/AnalyticsServiceProvider.php
@@ -15,7 +15,7 @@ class AnalyticsServiceProvider extends ServiceProvider
 
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'analytics');
     }
